68bfaf07e0ad8c88ee60b534ba96c8307cba7094
volar <vali.olar@gmail.com>
2017-03-06 13:36:04 -0500

    adding service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,33 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Hero } from './hero';
-
-export const HEROES: Hero[] = [
-    { id: 11, name: 'Mr. Nice' },
-    { id: 12, name: 'Narco' },
-    { id: 13, name: 'Bombasto' },
-    { id: 14, name: 'Celeritas' },
-    { id: 15, name: 'Magneta' },
-    { id: 16, name: 'RubberMan' },
-    { id: 17, name: 'Dynama' },
-    { id: 18, name: 'Dr IQ' },
-    { id: 19, name: 'Magma' },
-    { id: 20, name: 'Tornado' }
-];
-
+import { HeroService } from './hero.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers: [HeroService]
 })
 
-export class AppComponent {
-	title = 'Tour of Heroes';
-	heroes = HEROES;
-	selectedHero: Hero;
+export class AppComponent implements OnInit{
 
-	onSelect(hero: Hero): void {
-		this.selectedHero = hero;
-	}
+  ngOnInit(): void {
+    this.getHeroes();
+  }
+
+  title = 'Tour of Heroes';
+  heroes: Hero[];
+  selectedHero: Hero;
+
+  constructor(private heroService: HeroService) { }
+
+  getHeroes(): void {
+    this.heroes = this.heroService.getHeroes();
+  }
+
+  onSelect(hero: Hero): void {
+      this.selectedHero = hero;
+  }
+
 }
-
